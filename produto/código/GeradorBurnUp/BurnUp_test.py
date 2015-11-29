e81acbe971b2b3327d3d3acf11ee694962508bfc
philippe28 <phadc@hotmail.com>
2015-11-29 14:41:00 -0200

    AttributeError: Sprint instance has no attribute 'add_meta'

--- produto/código/GeradorBurnUp/BurnUp_test.py
+++ produto/código/GeradorBurnUp/BurnUp_test.py
@@ -7,3 +7,11 @@ def test_inicio_padrao():
     assert 0 == len(sprint.meta)
     assert 0 == len(sprint.trabalho)
     assert 0 != sprint.sprint
+
+
+def test_add_meta():
+    sprint = Sprint()
+    sprint.add_meta(100)
+
+    assert 1 == len(sprint.meta)
+    assert 100 == sprint.meta[0]
